4c6146ac921bf292a7f45a06d709e3b16e7d48a4
krishnayadavvv00 <krishnayadavvv00@gmail.com>
2026-04-06 13:27:59 +0530

    feat: add react-router-dom for routing and update Navbar component with social links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import {Linkedin} from 'lucide-react'
-import {Github} from 'lucide-react'
-import {Instagram} from 'lucide-react'
-import {Twitter} from 'lucide-react'
+import { Linkedin, Github, Instagram, Twitter } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <nav className=' mb-20 flex items-center justify-between py-6'>
-    <div className="flex `flex-shrink-0` items-center">
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      
+      <div className="flex flex-shrink-0 items-center">
         <h1 className='text-white mx-2 font-bold text-3xl'>Kr</h1>
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <Github />
-      <Instagram />
-      <Linkedin />
-      <Twitter />
-    </div>
+      </div>
+
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+
+        <a href="https://github.com/krishnayadavv1647" target="_blank" rel="noopener noreferrer">
+          <Github className='cursor-pointer hover:text-gray-400 transition'/>
+        </a>
+
+        <a href="https://www.instagram.com/krishna_.1_/?hl=en" target="_blank" rel="noopener noreferrer">
+          <Instagram className='cursor-pointer hover:text-pink-400 transition'/>
+        </a>
+
+        <a href="https://linkedin.com/in/KrishnaYadavv" target="_blank" rel="noopener noreferrer">
+          <Linkedin className='cursor-pointer hover:text-blue-400 transition'/>
+        </a>
+
+        <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+          <Twitter className='cursor-pointer hover:text-sky-400 transition'/>
+        </a>
+
+      </div>
      
     </nav>
   )
